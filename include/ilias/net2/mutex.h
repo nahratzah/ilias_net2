e795f178a268f6d8895a203c8863a1fdc29099d8
ariane <ariane@conformal.com>
2012-01-28 10:46:08 +0100

    Implement condition support.

--- include/ilias/net2/mutex.h
+++ include/ilias/net2/mutex.h
@@ -4,6 +4,7 @@
 #include <ilias/net2/ilias_net2_export.h>
 
 struct net2_mutex;
+struct net2_condition;
 
 #ifdef ilias_net2_EXPORTS
 ILIAS_NET2_LOCAL
@@ -14,6 +15,15 @@ ILIAS_NET2_LOCAL
 void			 net2_mutex_lock(struct net2_mutex*);
 ILIAS_NET2_LOCAL
 void			 net2_mutex_unlock(struct net2_mutex*);
+
+ILIAS_NET2_LOCAL
+struct net2_condition	*net2_cond_alloc();
+ILIAS_NET2_LOCAL
+void			 net2_cond_free(struct net2_condition*);
+ILIAS_NET2_LOCAL
+void			 net2_cond_signal(struct net2_condition*);
+ILIAS_NET2_LOCAL
+void			 net2_cond_broadcast(struct net2_condition*);
 #endif /* ilias_net2_EXPORTS */
 
 #endif /* ILIAS_NET2_MUTEX_H */
--- src/mutex-pthreads.c
+++ src/mutex-pthreads.c
@@ -7,7 +7,11 @@
 #include <errno.h>
 
 struct net2_mutex {
-	pthread_mutex_t n2m_impl;
+	pthread_mutex_t	n2m_impl;
+};
+
+struct net2_condition {
+	pthread_cond_t	n2c_impl;
 };
 
 /*
@@ -22,7 +26,7 @@ net2_mutex_alloc()
 	if ((m = malloc(sizeof(*m))) == NULL)
 		return m;
 	if ((rv = pthread_mutex_init(&m->n2m_impl, NULL)) != 0) {
-		warn("%s: %s", "pthread_mutex_destroy", strerror(rv));
+		warnx("%s: %s", "pthread_mutex_init", strerror(rv));
 		free(m);
 		return NULL;
 	}
@@ -85,4 +89,67 @@ net2_mutex_unlock(struct net2_mutex *m)
 			    strerror(rv));
 		}
 	}
+}
+
+
+
+/*
+ * Allocate a condition variable.
+ */
+ILIAS_NET2_LOCAL struct net2_condition*
+net2_cond_alloc()
+{
+	struct net2_condition	*c;
+	int			 rv;
+
+	if ((c = malloc(sizeof(c))) == NULL)
+		return c;
+
+	if ((rv = pthread_cond_init(&c->n2c_impl, NULL)) != 0) {
+		warnx("%s: %s", "pthread_cond_init", strerror(rv));
+		free(c);
+		return NULL;
+	}
+	return c;
+}
+
+/*
+ * Free a condition variable.
+ */
+ILIAS_NET2_LOCAL void
+net2_cond_free(struct net2_condition *c)
+{
+	int rv;
+
+	if (c == NULL)
+		return;
+	if ((rv = pthread_cond_destroy(&c->n2c_impl)) != 0) {
+		errx(EX_OSERR, "%s: %s",
+		    "pthread_cond_destroy", strerror(rv));
+	}
+	free(c);
+}
+
+/*
+ * Signal a condition variable, waking up a single waiting thread.
+ */
+ILIAS_NET2_LOCAL void
+net2_cond_signal(struct net2_condition *c)
+{
+	int rv;
+
+	if ((rv = pthread_cond_signal(&c->n2c_impl)) != 0)
+		warnx("%s: %s", "pthread_cond_signal", strerror(rv));
+}
+
+/*
+ * Wakeup all threads waiting for a condition.
+ */
+ILIAS_NET2_LOCAL void
+net2_cond_broadcast(struct net2_condition *c)
+{
+	int rv;
+
+	if ((rv = pthread_cond_broadcast(&c->n2c_impl)) != 0)
+		warnx("%s: %s", "pthread_cond_signal", strerror(rv));
 }
