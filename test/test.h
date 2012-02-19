8b0589dcdf7b4c375013d72b0eb2097780c60028
ariane <ariane@conformal.com>
2012-02-20 00:34:31 +0100

    Only pull in exude headers if enabled, test will now show if it's enabled.

--- test/test.h
+++ test/test.h
@@ -1,16 +1,23 @@
 #ifndef TEST__INIT_FINI
 #define TEST__INIT_FINI
 
+#ifdef NET2_USE_EXUDE_DEBUG
 #include <clog.h>
 #include <exude.h>
+#include <stdio.h>
+#endif /* NET2_USE_EXUDE_DEBUG */
 
 static __inline void
 test_start()
 {
 #ifdef NET2_USE_EXUDE_DEBUG
+	fprintf(stderr, "Setting up exude...\n");
 	clog_init(1);
-	clog_set_flags(CLOG_F_ENABLE | CLOG_F_STDERR);
+	clog_set_mask((uint64_t)-1);
+	clog_set_flags(CLOG_F_ENABLE | CLOG_F_STDERR | CLOG_F_FUNC | CLOG_F_LINE | CLOG_F_DTIME);
 	exude_enable(EXUDE_DBG_ALWAYS);
+#else
+	fprintf(stderr, "Not using exude...\n");
 #endif /* NET2_USE_EXUDE_DEBUG */
 }
 
