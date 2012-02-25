41400988ad30c9a8af082a5d0c88be84d697726f
ariane <ariane@conformal.com>
2012-02-25 14:36:07 +0100

    Fix: need stdio in all cases.

--- test/test.h
+++ test/test.h
@@ -1,10 +1,11 @@
 #ifndef TEST__INIT_FINI
 #define TEST__INIT_FINI
 
+#include <stdio.h>
+
 #ifdef NET2_USE_EXUDE_DEBUG
 #include <clog.h>
 #include <exude.h>
-#include <stdio.h>
 #endif /* NET2_USE_EXUDE_DEBUG */
 
 static __inline void
