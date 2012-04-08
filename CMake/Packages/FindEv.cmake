1e47516472de6b11f9c9e4613d9feeff4fb036f8
ariane <ariane@conformal.com>
2012-04-09 01:07:05 +0200

    Find libev.

---
+++ CMake/Packages/FindEv.cmake
@@ -0,0 +1,17 @@
+# - Try to find ev event library.
+# Once done, this will define
+#
+#  EV_FOUND - system has ev
+#  EV_INCLUDE_DIRS - the ev include dirs
+#  EV_LIBRARIES - link these to use ev
+
+include (FindPackageHandleStandardArgs)
+
+# Import EV_HOME from environment.
+set (EV_PREFIX_PATH ${EV_HOME})
+set (EV_LIBRARY_NAMES ev)
+
+find_path (EV_INCLUDE_DIR ev.h PATH_SUFFIXES include DOC "The path to the directory containing ev.h")
+find_library (EV_LIBRARIES NAMES ev PATH_SUFFIXES lib64 lib lib32 DOC "The ev library.")
+
+find_package_handle_standard_args (EV DEFAULT_MSG EV_LIBRARIES EV_INCLUDE_DIR)
