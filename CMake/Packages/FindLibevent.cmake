85cc5b5de732e9f646fa298b9985eaa9a09632d9
ariane <ariane@conformal.com>
2012-05-18 04:35:00 +0200

    More cmake rules.

--- CMake/Packages/FindLibevent.cmake
+++ CMake/Packages/FindLibevent.cmake
@@ -11,13 +11,12 @@ include (FindPackageHandleStandardArgs)
 set (LIBEVENT_PREFIX_PATH ${LIBEVENT_HOME})
 set (LIBEVENT_LIBRARY_NAMES event_core event_pthreads)
 
-find_path (LIBEVENT_INCLUDE_DIR event2/event.h PATH_SUFFIXES include DOC "The path to the directory containing event.h")
-find_library (LIBEVENT_core_LIBRARY NAMES event_core PATH_SUFFIXES lib64 lib lib32 DOC "The libevent core library.")
+find_path (LIBEVENT_INCLUDE_DIRS event2/event.h PATH_SUFFIXES include DOC "The path to the directory containing event.h")
+find_library (LIBEVENT_LIBRARIES NAMES event_core PATH_SUFFIXES lib64 lib lib32 DOC "The libevent core library.")
+
 find_library (LIBEVENT_thread_LIBRARY NAMES event_pthreads PATH_SUFFIXES lib64 lib lib32 DOC "The libevent thread library.")
-
-set (LIBEVENT_LIBRARIES ${LIBEVENT_core_LIBRARY})
-if (LIBEVENT_thread_LIBRARY)
+if (NOT LIBEVENT_thread_LIBRARY-NOTFOUND)
 	list (APPEND LIBEVENT_LIBRARIES ${LIBEVENT_thread_LIBRARY})
-endif (LIBEVENT_thread_LIBRARY)
+endif (NOT LIBEVENT_thread_LIBRARY-NOTFOUND)
 
-find_package_handle_standard_args (libEvent DEFAULT_MSG LIBEVENT_LIBRARIES LIBEVENT_INCLUDE_DIR)
+find_package_handle_standard_args (libEvent DEFAULT_MSG LIBEVENT_LIBRARIES LIBEVENT_INCLUDE_DIRS)
