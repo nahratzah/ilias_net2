fb61a998049a8e694018e63a9c548fbd686f65e3
ariane <ariane@conformal.com>
2012-05-18 04:12:44 +0200

    Handle non-found pthread lib.

--- CMake/Packages/FindLibevent.cmake
+++ CMake/Packages/FindLibevent.cmake
@@ -14,6 +14,10 @@ set (LIBEVENT_LIBRARY_NAMES event_core event_pthreads)
 find_path (LIBEVENT_INCLUDE_DIR event2/event.h PATH_SUFFIXES include DOC "The path to the directory containing event.h")
 find_library (LIBEVENT_core_LIBRARY NAMES event_core PATH_SUFFIXES lib64 lib lib32 DOC "The libevent core library.")
 find_library (LIBEVENT_thread_LIBRARY NAMES event_pthreads PATH_SUFFIXES lib64 lib lib32 DOC "The libevent thread library.")
-set (LIBEVENT_LIBRARIES ${LIBEVENT_core_LIBRARY} ${LIBEVENT_thread_LIBRARY})
+
+set (LIBEVENT_LIBRARIES ${LIBEVENT_core_LIBRARY})
+if (LIBEVENT_thread_LIBRARY)
+	list (APPEND LIBEVENT_LIBRARIES ${LIBEVENT_thread_LIBRARY})
+endif (LIBEVENT_thread_LIBRARY)
 
 find_package_handle_standard_args (libEvent DEFAULT_MSG LIBEVENT_LIBRARIES LIBEVENT_INCLUDE_DIR)
