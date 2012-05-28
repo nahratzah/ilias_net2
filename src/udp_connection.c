47337c2e51ebe14282e60aba017f3474bce5d169
ariane <ariane@conformal.com>
2012-05-28 15:25:55 +0200

    Switch to workq_io.

--- src/udp_connection.c
+++ src/udp_connection.c
@@ -18,13 +18,14 @@
 #include <ilias/net2/acceptor.h>
 #include <ilias/net2/mutex.h>
 #include <ilias/net2/sockdgram.h>
+#include <ilias/net2/workq.h>
+#include <ilias/net2/workq_io.h>
 #include <ilias/net2/buffer.h>
 #include <ilias/net2/memory.h>
 #include <errno.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <event2/event.h>
 #ifdef WIN32
 #include <WinSock2.h>
 #include <io.h>
@@ -43,28 +44,29 @@
 
 struct net2_conn_p2p;
 
-/* Read at most this many packets per read. */
-#define DEQUEUE_MAX		256
+ILIAS_NET2_LOCAL
+void	 net2_conn_p2p_destroy(struct net2_acceptor_socket*);
+ILIAS_NET2_LOCAL
+void	 net2_conn_p2p_ready_to_send(struct net2_acceptor_socket*);
 
 ILIAS_NET2_LOCAL
-void	net2_conn_p2p_destroy(struct net2_acceptor_socket*);
+void	 net2_conn_p2p_recv(void*, struct net2_dgram_rx*);
 ILIAS_NET2_LOCAL
-void	net2_conn_p2p_ready_to_send(struct net2_acceptor_socket*);
+struct net2_promise
+	*net2_conn_p2p_send(void*, size_t);
 ILIAS_NET2_LOCAL
-int	net2_conn_p2p_setev(struct net2_conn_p2p*, short);
+void	 net2_udpsocket_recv(void*, struct net2_dgram_rx*);
+ILIAS_NET2_LOCAL
+struct net2_promise
+	*net2_udpsocket_send(void*, size_t);
 
 ILIAS_NET2_LOCAL
-void	net2_conn_p2p_recv(int, short, void*);
+void	 net2_udpsock_unlock(struct net2_udpsocket*);
 ILIAS_NET2_LOCAL
-void	net2_udpsocket_recv(int, short, void*);
-
-ILIAS_NET2_LOCAL
-void	net2_udpsock_unlock(struct net2_udpsocket*);
-ILIAS_NET2_LOCAL
-void	net2_udpsock_conn_unlink(struct net2_udpsocket*,
+void	 net2_udpsock_conn_unlink(struct net2_udpsocket*,
 	    struct net2_conn_p2p*);
 ILIAS_NET2_LOCAL
-void	net2_udpsock_conn_wantsend(struct net2_udpsocket*,
+void	 net2_udpsock_conn_wantsend(struct net2_udpsocket*,
 	    struct net2_conn_p2p*);
 
 
@@ -83,7 +85,7 @@ struct net2_conn_p2p {
 	int			 np2p_sock;	/* UDP socket. */
 	struct sockaddr		*np2p_remote;	/* Remote address. */
 	socklen_t		 np2p_remotelen; /* Remote address len. */
-	struct event		*np2p_ev;	/* Send/receive event. */
+	struct net2_workq_io	*np2p_ev;	/* Send/receive event. */
 	struct net2_udpsocket	*np2p_udpsock;	/* Shared UDP socket. */
 
 	RB_ENTRY(net2_conn_p2p)	 np2p_socktree;	/* Link into udp socket. */
@@ -102,10 +104,10 @@ RB_HEAD(net2_udpsocket_conns, net2_conn_p2p);
  * its connection queue becomes empty.
  */
 struct net2_udpsocket {
-	evutil_socket_t		 sock;		/* Socket. */
+	net2_socket_t		 sock;		/* Socket. */
 	size_t			 refcnt;	/* Reference counter. */
 	struct net2_mutex	*guard;		/* Protect against races. */
-	struct event		*ev;		/* Send/receive event. */
+	struct net2_workq_io	*ev;		/* Send/receive event. */
 	struct net2_workq	*workq;		/* Thread context. */
 
 	struct net2_udpsocket_conns
@@ -182,7 +184,7 @@ RB_GENERATE_STATIC(net2_udpsocket_conns, net2_conn_p2p, np2p_socktree,
  */
 ILIAS_NET2_EXPORT struct net2_connection*
 net2_conn_p2p_create_fd(struct net2_ctx *ctx,
-    struct net2_workq_evbase *wqev, evutil_socket_t sock,
+    struct net2_workq_evbase *wqev, net2_socket_t sock,
     struct sockaddr *remote, socklen_t remotelen)
 {
 	struct net2_conn_p2p	*c;
@@ -225,8 +227,7 @@ net2_conn_p2p_create_fd(struct net2_ctx *ctx,
 
 	/* Set up libevent network-receive event. */
 	c->np2p_ev = NULL;
-	if (net2_conn_p2p_setev(c, EV_READ))
-		goto fail_3;
+	net2_workq_io_activate_rx(c->np2p_ev);
 
 	return &c->np2p_conn;
 
@@ -294,7 +295,7 @@ ILIAS_NET2_EXPORT struct net2_udpsocket*
 net2_conn_p2p_socket(struct net2_workq_evbase *wqev, struct sockaddr *bindaddr,
     socklen_t bindaddrlen)
 {
-	evutil_socket_t		 fd = -1;
+	net2_socket_t		 fd = -1;
 	int			 saved_errno;
 	struct net2_udpsocket	*rv;
 	struct net2_workq	*wq = NULL;
@@ -327,17 +328,16 @@ net2_conn_p2p_socket(struct net2_workq_evbase *wqev, struct sockaddr *bindaddr,
 	rv->refcnt = 1;
 	if ((rv->guard = net2_mutex_alloc()) == NULL)
 		goto fail_guard;
-	rv->ev = event_new(evbase->evbase, fd, EV_READ|EV_PERSIST,
-	    &net2_udpsocket_recv, rv);
+	rv->ev = net2_workq_io_new(wq, fd, &net2_udpsocket_recv, &net2_udpsocket_send, rv);
 	if (rv->ev == NULL)
 		goto fail_event_new;
-	if (event_add(rv->ev, NULL))
-		goto fail_event_add;
 
+	net2_workq_io_activate_rx(rv->ev);
+	net2_workq_io_activate_tx(rv->ev);
 	return rv;
 
 fail_event_add:
-	event_del(rv->ev);
+	net2_workq_io_destroy(rv->ev);
 fail_event_new:
 	net2_mutex_free(rv->guard);
 fail_guard:
@@ -394,10 +394,8 @@ net2_conn_p2p_destroy(struct net2_acceptor_socket *cptr)
 #endif
 	if (udpsock != NULL)
 		net2_udpsock_conn_unlink(udpsock, c);
-	if (c->np2p_ev) {
-		event_del(c->np2p_ev);
-		event_free(c->np2p_ev);
-	}
+	if (c->np2p_ev)
+		net2_workq_io_destroy(c->np2p_ev);
 	net2_connection_deinit(&c->np2p_conn);
 	net2_free(cptr);
 }
@@ -412,10 +410,8 @@ net2_conn_p2p_ready_to_send(struct net2_acceptor_socket *cptr)
 
 	if (c->np2p_udpsock != NULL)
 		net2_udpsock_conn_wantsend(c->np2p_udpsock, c);
-	else {
-		if (net2_conn_p2p_setev(c, EV_READ | EV_WRITE))
-			warnx("p2p connection: may lose data...");
-	}
+	else
+		net2_workq_io_activate_tx(c->np2p_ev);
 }
 
 /*
@@ -423,232 +419,127 @@ net2_conn_p2p_ready_to_send(struct net2_acceptor_socket *cptr)
  * Only to be used on connected datagram sockets.
  */
 ILIAS_NET2_LOCAL void
-net2_conn_p2p_recv(int sock, short what, void *cptr)
+net2_conn_p2p_recv(void *cptr, struct net2_dgram_rx *rx)
 {
 	struct net2_conn_p2p	*c = cptr;
 	struct net2_conn_receive*r;
-	int			 dequeued;
-	struct net2_buffer	*buf;
-	int			 rv;
-	size_t			 wire_sz;
 
-	assert(c->np2p_sock == sock);
+	if ((r = net2_malloc(sizeof(*r))) == NULL)
+		return;	/* Drop packet. */
+	r->buf = rx->data;
+	rx->data = NULL;
+	r->error = rx->error;
 
-	/*
-	 * Read network input.
-	 */
-	if (what & EV_READ) {
-		for (dequeued = 0; dequeued < DEQUEUE_MAX; dequeued++) {
-			if (net2_sockdgram_recv(sock, &r, NULL, 0)) {
-				/* TODO: kill connection */
-				return;
-			}
-
-			/* GUARD */
-			if (r == NULL)
-				break;
-			net2_connection_recv(&c->np2p_conn, r);
-		}
-	}
-
-	/*
-	 * Write network data.
-	 */
-	if (what & EV_WRITE) {
-		wire_sz = c->np2p_conn.n2c_stats.wire_sz;
-		if (secure_random_uniform(16) == 0) {
-			if (c->np2p_conn.n2c_stats.over_sz == 0)
-				wire_sz *= 2;
-			else {
-				/*
-				 * Take the average between largest ack and
-				 * smallest nack.
-				 */
-				wire_sz += c->np2p_conn.n2c_stats.over_sz;
-				wire_sz /= 2;
-			}
-		}
-
-		buf = NULL;
-		if (net2_conn_gather_tx(&c->np2p_conn, &buf, wire_sz)) {
-			/* TODO: kill connection */
-			return;
-		}
-		if (buf == NULL) {
-			/* Nothing to transmit, turn off write event. */
-			net2_conn_p2p_setev(c, EV_READ);
-			return;
-		}
-
-		rv = net2_sockdgram_send(sock, &c->np2p_conn, buf,
-		    c->np2p_remote, c->np2p_remotelen);
-		net2_buffer_free(buf);
-		switch (rv) {
-		case NET2_CONNFAIL_OK:
-			break;
-		case NET2_CONNFAIL_CLOSE:
-			/* TODO kill connection */
-			break;
-		case NET2_CONNFAIL_TOOBIG:
-			/* TODO: figure out what to do here,
-			 * better yet, try to avoid it */
-			break;
-		case NET2_CONNFAIL_BAD:
-			/* TODO: kill connection */
-			break;
-		case NET2_CONNFAIL_OS:
-			/*
-			 * Drop packet and let retransmission logic deal
-			 * with it.
-			 */
-			break;
-		case NET2_CONNFAIL_IO:
-			/* Drop packet. */
-			break;
-		case NET2_CONNFAIL_RESOURCE:
-			/*
-			 * Out of memory... maybe kill this connection to make
-			 * room? Or find a sacrificial goat somewhere...
-			 */
-			break;
-		default:
-			errx(EX_SOFTWARE, "net2_sockdgram_send "
-			    "unexpected response %d", rv);
-		}
-	}
+	net2_connection_recv(&c->np2p_conn, r);
 }
 
-/* Event initialization for connection-based events. */
-ILIAS_NET2_LOCAL int
-net2_conn_p2p_setev(struct net2_conn_p2p *c, short what)
+ILIAS_NET2_LOCAL struct net2_promise*
+net2_conn_p2p_send(void *cptr, size_t maxsz)
 {
-	struct net2_evbase	*evbase;
-	int			 allocated = 0;
-	int			 want_write, have_write;
-	int			 want_read,  have_read;
+	struct net2_conn_p2p	*c = cptr;
+	struct net2_buffer	*buf;
+	size_t			 wire_sz;
+	struct net2_dgram_tx_promdata
+				*pd;
+	struct net2_promise	*p;
 
-	assert(c->np2p_udpsock == NULL);
-	evbase = net2_acceptor_socket_evbase(&c->np2p_conn.n2c_socket);
-	want_write = (what & EV_WRITE);
-	want_read = (what & EV_READ);
-
-	if (c->np2p_ev == NULL) {
-		if ((c->np2p_ev = event_new(evbase->evbase,
-		    c->np2p_sock, what | EV_PERSIST,
-		    net2_conn_p2p_recv, c)) == NULL) {
-			warnx("event_new fail");
-			return -1;
-		}
-		allocated = 1;
-	} else {
-		/*
-		 * Check if the event is already pending for the right
-		 * event types.
-		 */
-		have_write = event_pending(c->np2p_ev, EV_WRITE, NULL);
-		have_read  = event_pending(c->np2p_ev, EV_READ, NULL);
-		if ((have_write == 0) == (want_write == 0) &&
-		    (have_read  == 0) == (want_read  == 0)) {
-			return 0;
-		}
-
-		/* Remove and reinitialize event. */
-		event_free(c->np2p_ev);
-		if ((c->np2p_ev = event_new(evbase->evbase,
-		    c->np2p_sock, what | EV_PERSIST,
-		    net2_conn_p2p_recv, c)) == NULL) {
-			warnx("event_new fail");
-			goto fail;
+	wire_sz = c->np2p_conn.n2c_stats.wire_sz;
+	if (secure_random_uniform(16) == 0) {
+		if (c->np2p_conn.n2c_stats.over_sz == 0)
+			wire_sz *= 2;
+		else {
+			/*
+			 * Take the average between largest ack and
+			 * smallest nack.
+			 */
+			wire_sz += c->np2p_conn.n2c_stats.over_sz;
+			wire_sz /= 2;
 		}
 	}
 
-	/* Add event. */
-	if (event_add(c->np2p_ev, NULL)) {
-		warnx("event_add fail");
-		goto fail;
-	}
-	return 0;
+	wire_sz = MIN(wire_sz, maxsz);
 
-fail:
-	if (allocated) {
-		event_free(c->np2p_ev);
-		c->np2p_ev = NULL;
+	buf = NULL;
+	if (net2_conn_gather_tx(&c->np2p_conn, &buf, wire_sz)) {
+		/* TODO: kill connection */
+		return NULL;
 	}
-	return -1;
+	if (buf == NULL)
+		return NULL;
+
+	if ((pd = net2_malloc(sizeof(*pd))) == NULL) {
+		net2_buffer_free(buf);
+		return NULL;
+	}
+	pd->data = buf;
+	buf = NULL;
+	pd->addrlen = 0;
+	pd->tx_done = NULL;
+
+	if ((p = net2_promise_new()) == NULL) {
+		net2_workq_io_tx_pdata_free(pd, NULL);
+		return NULL;
+	}
+	if (net2_promise_set_finok(p, pd, &net2_workq_io_tx_pdata_free,
+	    NULL, 0) != 0) {
+		net2_promise_release(p);
+		net2_workq_io_tx_pdata_free(pd, NULL);
+		return NULL;
+	}
+
+	return p;
 }
 
 /*
  * UDP specific implementation of receive.
  */
 ILIAS_NET2_LOCAL void
-net2_udpsocket_recv(int sock, short what, void *udps_ptr)
+net2_udpsocket_recv(void *udps_ptr, struct net2_dgram_rx *rx)
 {
 	struct net2_udpsocket	*udps = udps_ptr;
 	struct net2_conn_receive*r;
-	struct sockaddr_storage	 from;
-	socklen_t		 fromlen;
 	struct net2_conn_p2p	*c, search;
-	int			 dequeued;
+
+	if ((r = net2_malloc(sizeof(*r))) == NULL)
+		return;	/* Drop packet. */
+	r->buf = rx->data;
+	rx->data = NULL;
+	r->error = rx->error;
+
+	search.np2p_remote = (struct sockaddr*)&rx->addr;
+	search.np2p_remotelen = rx->addrlen;
+	c = RB_FIND(net2_udpsocket_conns, &udps->conns, &search);
+	if (c != NULL)
+		net2_connection_recv(&c->np2p_conn, r);
+	else {
+		/* Unrecognized connection. */
+		/* XXX Implement new connection callback. */
+		if (r->buf)
+			net2_buffer_free(r->buf);
+		net2_free(r);
+	}
+}
+
+ILIAS_NET2_LOCAL struct net2_promise*
+net2_udpsocket_send(void *udps_ptr, size_t maxsz)
+{
+	struct net2_udpsocket	*udps = udps_ptr;
+	struct net2_conn_p2p	*c;
 	size_t			 wire_sz;
 	struct net2_buffer	*buf;
-	int			 rv;
+	struct net2_dgram_tx_promdata
+				*pd;
+	struct net2_promise	*p;
 
-	assert(udps->sock == sock);
+	if ((pd = net2_malloc(sizeof(*pd))) == NULL)
+		return NULL;	/* TODO: figure out a way to return ENOMEM. */
 
-	/*
-	 * Read network input.
-	 */
-	if (what & EV_READ) {
-		for (dequeued = 0; dequeued < DEQUEUE_MAX; dequeued++) {
-			fromlen = sizeof(from);
-			if (net2_sockdgram_recv(sock, &r,
-			    (struct sockaddr*)&from, &fromlen)) {
-				/* TODO: kill socket */
-				return;
-			}
-
-			/* GUARD */
-			if (r == NULL)
-				break;
-
-			/* Find connection. */
-			search.np2p_remote = (struct sockaddr*)&from;
-			search.np2p_remotelen = fromlen;
-			net2_mutex_lock(udps->guard);	/* LOCK */
-			c = RB_FIND(net2_udpsocket_conns, &udps->conns,
-			    &search);
-
-			/* Deliver data to connection. */
-			if (c != NULL)
-				net2_connection_recv(&c->np2p_conn, r);
-			else {
-				/* Unrecognized connection. */
-				/* TODO: implement new connection callback */
-				if (r->buf)
-					net2_buffer_free(r->buf);
-				net2_free(r);
-			}
-			net2_mutex_unlock(udps->guard);	/* UNLOCK */
-		}
-	}
-
-	/*
-	 * Invoke write callback
-	 */
-	if (what & EV_WRITE) {
-retry_tx:
-		net2_mutex_lock(udps->guard);	/* LOCK */
-
-		if ((c = TAILQ_FIRST(&udps->wantwrite)) == NULL)
-			goto empty_sendq;	/* Shouldn't happen. */
-
-		/* Remove c from writeq. */
+	/* Find something to transmit. */
+	while ((c = TAILQ_FIRST(&udps->wantwrite)) != NULL) {
 		TAILQ_REMOVE(&udps->wantwrite, c, np2p_wantwriteq);
-		c->np2p_flags &= ~NP2P_F_SENDQ;
-
-		net2_mutex_unlock(udps->guard);	/* UNLOCK */
-
 		wire_sz = c->np2p_conn.n2c_stats.wire_sz;
+
+		/* Attempt packet growth. */
 		if (secure_random_uniform(16) == 0) {
 			if (c->np2p_conn.n2c_stats.over_sz == 0)
 				wire_sz *= 2;
@@ -661,75 +552,45 @@ retry_tx:
 				wire_sz /= 2;
 			}
 		}
+		wire_sz = MIN(wire_sz, maxsz);
 
 		buf = NULL;
 		if (net2_conn_gather_tx(&c->np2p_conn, &buf, wire_sz)) {
 			/* TODO: kill connection */
-			return;
+			continue;
 		}
-		if (buf == NULL) {
-			/* Nothing to transmit, try another connection. */
-			goto retry_tx;
+		if (buf != NULL) {
+			/* Found something to transmit. */
+			break;
 		}
-
-		rv = net2_sockdgram_send(sock, &c->np2p_conn, buf,
-		    c->np2p_remote, c->np2p_remotelen);
-		net2_buffer_free(buf);
-		switch (rv) {
-		case NET2_CONNFAIL_OK:
-			break;
-		case NET2_CONNFAIL_CLOSE:
-			/* TODO kill connection */
-			break;
-		case NET2_CONNFAIL_TOOBIG:
-			/* TODO: figure out what to do here,
-			 * better yet, try to avoid it */
-			break;
-		case NET2_CONNFAIL_BAD:
-			/* TODO kill connection */
-			break;
-		case NET2_CONNFAIL_OS:
-			/*
-			 * Drop packet and let retransmission logic deal
-			 * with it.
-			 */
-			break;
-		case NET2_CONNFAIL_IO:
-			/* Drop packet. */
-			break;
-		case NET2_CONNFAIL_RESOURCE:
-			/*
-			 * Out of memory... maybe kill this connection to make
-			 * room? Or find a sacrificial goat somewhere...
-			 */
-			break;
-		default:
-			errx(EX_SOFTWARE, "net2_sockdgram_send "
-			    "unexpected response %d", rv);
-		}
-
-		net2_mutex_lock(udps->guard);	/* LOCK */
-
-		/* Put c back on the sendq, if it hasn't done so itself. */
-		if (c != NULL && !(c->np2p_flags & NP2P_F_SENDQ)) {
-			c->np2p_flags |= NP2P_F_SENDQ;
-			TAILQ_INSERT_TAIL(&udps->wantwrite, c, np2p_wantwriteq);
-		}
-
-		/* Test if we still have pending writes. */
-		if (TAILQ_EMPTY(&udps->wantwrite)) {
-empty_sendq:
-			event_free(udps->ev);
-			if ((udps->ev = event_new(udps->evbase->evbase, sock,
-			    EV_READ|EV_PERSIST, &net2_udpsocket_recv, udps)) ==
-			    NULL)
-				warnx("event_new fail");
-			if (event_add(udps->ev, NULL))
-				warnx("event_add fail");
-		}
-
-		net2_mutex_unlock(udps->guard);	/* UNLOCK */
 	}
+	/* Nothing to transmit. */
+	if (c == NULL) {
+		net2_free(pd);
+		return NULL;
+	}
+
+	assert(c->np2p_remotelen <= sizeof(pd->addrlen));
+	memcpy(&pd->addr, c->np2p_remote, c->np2p_remotelen);
+	pd->addrlen = c->np2p_remotelen;
+	pd->data = buf;
+	buf = NULL;
+	pd->tx_done = NULL;
+
+	/* Put c back on the sendq, if it hasn't done so itself. */
+	if (c != NULL && !(c->np2p_flags & NP2P_F_SENDQ)) {
+		c->np2p_flags |= NP2P_F_SENDQ;
+		TAILQ_INSERT_TAIL(&udps->wantwrite, c, np2p_wantwriteq);
+	}
+
+	p = net2_promise_new();
+	if (net2_promise_set_finok(p, pd, &net2_workq_io_tx_pdata_free,
+	    NULL, 0) != 0) {
+		net2_promise_release(p);
+		net2_workq_io_tx_pdata_free(pd, NULL);
+		return NULL;
+	}
+	return p;
 }
 
 /* Unlock socket and remove it if it has no references. */
@@ -737,17 +598,20 @@ ILIAS_NET2_LOCAL void
 net2_udpsock_unlock(struct net2_udpsocket *sock)
 {
 	int		do_rm;
+	int		want_err;
 
 	do_rm = RB_EMPTY(&sock->conns) &&
-	    sock->refcnt == 0;
-	if (do_rm)
-		event_del(sock->ev);
+	    sock->refcnt == 0; /* XXX and no acceptor fn */
 	net2_mutex_unlock(sock->guard);
 
 	if (do_rm) {
+		want_err = net2_workq_want(sock->workq, 0);
+		assert(want_err == 0 || want_err == EDEADLK);
+		net2_workq_io_destroy(sock->ev);
 		net2_mutex_free(sock->guard);
-		event_free(sock->ev);
-		net2_evbase_release(sock->evbase);
+		if (want_err == 0)
+			net2_workq_unwant(sock->workq);
+		net2_workq_release(sock->workq);
 		net2_free(sock);
 	}
 }
@@ -772,15 +636,7 @@ net2_udpsock_conn_wantsend(struct net2_udpsocket *sock, struct net2_conn_p2p *c)
 	net2_mutex_lock(sock->guard);
 
 	/* Modify event to be pending for write. */
-	if (!event_pending(sock->ev, EV_WRITE, NULL)) {
-		if (event_del(sock->ev))
-			errx(EX_UNAVAILABLE, "event_del fail");
-		if (event_assign(sock->ev, sock->evbase->evbase, sock->sock,
-		    EV_READ|EV_WRITE|EV_PERSIST, &net2_udpsocket_recv, sock))
-			warnx("event_assign fail");
-		if (event_add(sock->ev, NULL))
-			warnx("event_add fail");
-	}
+	net2_workq_io_activate_tx(sock->ev);
 
 	/* Add connection to wantwriteq. */
 	assert(RB_FIND(net2_udpsocket_conns, &sock->conns, c) == c);
