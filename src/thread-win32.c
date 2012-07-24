f4a00fef7b694926e771e79201205049863c58ce
Ariane van der Steldt <ariane@stack.nl>
2012-07-24 04:16:56 +0200

    Fix windows thread abstraction.

--- src/thread-win32.c
+++ src/thread-win32.c
@@ -20,52 +20,96 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include <process.h>
+#include <assert.h>
+#include <errno.h>
 
-#include <ilias/net2/config.h>
-#ifdef HAVE_SYS_TREE_H
-#include <sys/tree.h>
-#else
-#include <ilias/net2/bsd_compat/tree.h>
-#endif
+enum state {
+	ATTACHED,	/* Collect me or detach me. */
+	ATTACHED_FREE,	/* Go to DETACHED_FREE. */
+	DETACHED,	/* Don't collect me: HANDLE = NULL. */
+	DETACHED_FREE,	/* Thread is last owner: HANDLE = NULL. */
+	DYING,		/* Collect me. */
+	DEAD,		/* Collected or detached dead thread: HANDLE = null */
+	FAKE		/* Not a thread: HANDLE = null */
+};
 
 struct net2_thread {
+	CRITICAL_SECTION s;
+	enum state	 state;
+	unsigned	 tid;
 	HANDLE		 handle;
 	void		*result;
 	void		*(*fn)(void*);
 	void		*arg;
-	unsigned	 tid;
-	int		 detached;
-
-	RB_ENTRY(net2_thread)
-			 rb;
 };
 
-/* Store threads in a set, based on the tid; s is the protector. */
-static CRITICAL_SECTION s;
-static RB_HEAD(win32_threads, net2_thread) global = RB_INITIALIZER(&global);
-
-static __inline int
-tid_cmp(struct net2_thread *t1, struct net2_thread *t2)
-{
-	return (t1->tid < t2->tid ? -1 : t1->tid > t2->tid);
-}
-
-RB_PROTOTYPE_STATIC(win32_threads, net2_thread, rb, tid_cmp);
+/* Store threads in thread local storage slot. */
+static DWORD tls_slot;
 
 static DWORD __stdcall
 thread_wrapper(void *tptr)
 {
 	struct net2_thread *t = tptr;
 
+	TlsSetValue(tls_slot, t);
 	t->result = t->fn(t->arg);
-	_endthreadex(0);
+	TlsSetValue(tls_slot, NULL);
 
 	/* Close the handle ourselves, if the thread is detached. */
-	EnterCriticalSection(&s);
-	if (t->detached)
+	EnterCriticalSection(&t->s);
+	switch (t->state) {
+	case ATTACHED:
+		t->state = DYING;
+		break;
+	case DETACHED:
+		t->state = DEAD;
+		LeaveCriticalSection(&t->s);
+		break;
+	case ATTACHED_FREE:
 		CloseHandle(t->handle);
-	LeaveCriticalSection(&s);
+		/* FALLTHROUGH */
+	case DETACHED_FREE:
+		/* We are the last owner of this. */
+		LeaveCriticalSection(&t->s);
+		DeleteCriticalSection(&t->s);
+		net2_free(t);
+		break;
+	default:
+		abort();
+	}
+	_endthreadex(0);
 	return 0;
+}
+
+static const DWORD MS_VC_EXCEPTION=0x406D1388;
+
+#pragma pack(push,8)
+typedef struct tagTHREADNAME_INFO
+{
+	DWORD dwType; // Must be 0x1000.
+	LPCSTR szName; // Pointer to name (in user addr space).
+	DWORD dwThreadID; // Thread ID (-1=caller thread).
+	DWORD dwFlags; // Reserved for future use, must be zero.
+} THREADNAME_INFO;
+#pragma pack(pop)
+
+static void
+SetThreadName(DWORD dwThreadID, char *threadName)
+{
+	THREADNAME_INFO info;
+	info.dwType = 0x1000;
+	info.szName = threadName;
+	info.dwThreadID = dwThreadID;
+	info.dwFlags = 0;
+
+	__try
+	{
+		RaiseException(MS_VC_EXCEPTION, 0,
+		    sizeof(info) / sizeof(ULONG_PTR), (ULONG_PTR*)&info);
+	}
+	__except(EXCEPTION_EXECUTE_HANDLER)
+	{
+	}
 }
 
 /*
@@ -87,8 +131,9 @@ net2_thread_new(void *(*fn)(void*), void *arg, const char *name)
 	t->fn = fn;
 	t->arg = arg;
 	t->result = NULL;
+	InitializeCriticalSection(&t->s);
+	t->state = ATTACHED;
 
-	EnterCriticalSection(&s);
 	t->handle = (HANDLE)_beginthreadex(NULL, 0, &thread_wrapper, t,
 	    CREATE_SUSPENDED, &t->tid);
 	/*
@@ -98,13 +143,14 @@ net2_thread_new(void *(*fn)(void*), void *arg, const char *name)
 	 */
 	if (t->handle == ((HANDLE)((uintptr_t)-1)) ||
 	    t->handle == ((HANDLE)((uintptr_t) 0))) {
-		LeaveCriticalSection(&s);
+		DeleteCriticalSection(&t->s);
 		net2_free(t);
 		return NULL;
 	}
 
-	RB_INSERT(win32_threads, &global, t);
-	LeaveCriticalSection(&s);
+	/* Assign thread name. */
+	SetThreadName(t->tid, (char*)name);
+	/* Start the thread now its thread structure is complete. */
 	ResumeThread(t->handle);
 
 	return t;
@@ -114,15 +160,22 @@ net2_thread_new(void *(*fn)(void*), void *arg, const char *name)
 ILIAS_NET2_LOCAL int
 net2_thread_join(struct net2_thread *t, void **out)
 {
+	/* Cannot join detached or fake threads. */
+	assert(t->tid != GetCurrentThreadId());
+	EnterCriticalSection(&t->s);
+	assert(t->state == ATTACHED || t->state == DYING);
+	LeaveCriticalSection(&t->s);
+
 	if (WaitForSingleObject(t->handle, INFINITE) != WAIT_OBJECT_0)
 		return -1;
+	EnterCriticalSection(&t->s);
+	assert(t->state == DYING);
+	t->state = DEAD;
+	LeaveCriticalSection(&t->s);
 	CloseHandle(t->handle);
+	t->handle = NULL;
 	if (out != NULL)
 		*out = t->result;
-	EnterCriticalSection(&s);
-	RB_REMOVE(win32_threads, &global, t);
-	t->detached = 1;
-	LeaveCriticalSection(&s);
 	return 0;
 }
 
@@ -130,11 +183,32 @@ net2_thread_join(struct net2_thread *t, void **out)
 ILIAS_NET2_LOCAL void
 net2_thread_free(struct net2_thread *t)
 {
-	EnterCriticalSection(&s);
-	if (!t->detached)
-		RB_REMOVE(win32_threads, &global, t);
-	LeaveCriticalSection(&s);
-	net2_free(t);
+	EnterCriticalSection(&t->s);
+	switch (t->state) {
+	case DYING:
+		/* We need to collect the thread. */
+		if (WaitForSingleObject(t->handle, INFINITE) != WAIT_OBJECT_0)
+			abort();
+		CloseHandle(t->handle);
+		t->state = DEAD;
+		/* FALLTHROUGH */
+	case DEAD:
+	case FAKE:
+		LeaveCriticalSection(&t->s);
+		net2_free(t);
+		return;
+	case ATTACHED:
+		t->state = ATTACHED_FREE;
+		break;
+	case DETACHED_FREE:
+		break;
+	case DETACHED:
+		t->state = DETACHED_FREE;
+		break;
+	case ATTACHED_FREE:
+		abort();
+	}
+	LeaveCriticalSection(&t->s);
 }
 
 /* Test if the given thread is the current thread. */
@@ -159,7 +233,8 @@ net2_thread_self()
 
 	if ((t = net2_malloc(sizeof(*t))) == NULL)
 		return NULL;
-	t->detached = 1;
+	InitializeCriticalSection(&t->s);
+	t->state = FAKE;
 	t->tid = GetCurrentThreadId();
 	return t;
 }
@@ -168,23 +243,34 @@ net2_thread_self()
 ILIAS_NET2_LOCAL void
 net2_thread_detach_self()
 {
-	struct net2_thread	*t, search;
+	struct net2_thread	*t = TlsGetValue(tls_slot);
 
-	search.tid = GetCurrentThreadId();
-	EnterCriticalSection(&s);
-	t = RB_FIND(win32_threads, &global, &search);
-	if (t != NULL && !t->detached) {
-		t->detached = 1;
-		RB_REMOVE(win32_threads, &global, t);
+	assert(t != NULL);
+	EnterCriticalSection(&t->s);
+	switch (t->state) {
+	case ATTACHED:
+		t->state = DETACHED;
+		break;
+	case ATTACHED_FREE:
+		t->state = DETACHED_FREE;
+		break;
+	case DETACHED:
+	case DETACHED_FREE:
+		/* Already detached, allow multi call. */
+		break;
+	default:
+		abort();
 	}
-	LeaveCriticalSection(&s);
+	LeaveCriticalSection(&t->s);
 }
 
 /* Initialize thread mutex. */
 ILIAS_NET2_LOCAL int
 net2_thread_init()
 {
-	InitializeCriticalSection(&s);
+	tls_slot = TlsAlloc();
+	if (tls_slot == TLS_OUT_OF_INDEXES)
+		return ENOMEM;
 	return 0;
 }
 
@@ -192,7 +278,5 @@ net2_thread_init()
 ILIAS_NET2_LOCAL void
 net2_thread_fini()
 {
-	DeleteCriticalSection(&s);
+	TlsFree(tls_slot);
 }
-
-RB_GENERATE_STATIC(win32_threads, net2_thread, rb, tid_cmp);
