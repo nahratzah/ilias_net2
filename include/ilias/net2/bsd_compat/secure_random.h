1b123448b2f9262632dacad09bbc003a657cdcc3
ariane <ariane@stack.nl>
2012-07-25 18:40:00 +0200

    Provide linux/generic unix secure random, using /dev/random.

--- include/ilias/net2/bsd_compat/secure_random.h
+++ include/ilias/net2/bsd_compat/secure_random.h
@@ -16,8 +16,8 @@
 #ifndef ILIAS_NET2_BSD_COMPAT_SECURE_RANDOM_H
 #define ILIAS_NET2_BSD_COMPAT_SECURE_RANDOM_H
 
-#include <ilias/net2/config.h>
 #include <ilias/net2/ilias_net2_export.h>
+#include <ilias/net2/config.h>
 #include <sys/types.h>
 
 #ifdef HAS_ARC4RANDOM
@@ -38,28 +38,42 @@
  */
 #include <stdint.h>
 
-#ifdef __cplusplus__
-extern "C" {
-#endif
-ILIAS_NET2_EXPORT
+ILIAS_NET2__begin_cdecl
+
+ILIAS_NET2_LOCAL
 uint32_t		win32_secure_random();
-ILIAS_NET2_EXPORT
+ILIAS_NET2_LOCAL
 void			win32_secure_random_buf(void*, size_t);
-ILIAS_NET2_EXPORT
+ILIAS_NET2_LOCAL
 uint32_t		win32_secure_random_uniform(uint32_t);
-#ifdef __cplusplus__
-}
-#endif
+
+ILIAS_NET2__end_cdecl
 
 #define secure_random()			win32_secure_random()
 #define secure_random_buf(_ptr, _len)	win32_secure_random_buf((_ptr), (_len))
 #define secure_random_uniform(_top)	win32_secure_random_uniform((_top))
 
 #else
+
 /*
- * OS needs configuration.
+ * Fallback to using /dev/random.
  */
-#error "No secure random implementation available."
+
+ILIAS_NET2__begin_cdecl
+
+ILIAS_NET2_LOCAL
+uint32_t		devrandom_secure_random();
+ILIAS_NET2_LOCAL
+void			devrandom_secure_random_buf(void*, size_t);
+ILIAS_NET2_LOCAL
+uint32_t		devrandom_secure_random_uniform(uint32_t);
+
+ILIAS_NET2__end_cdecl
+
+#define secure_random()			devrandom_secure_random()
+#define secure_random_buf(_ptr, _len)	devrandom_secure_random_buf((_ptr), (_len))
+#define secure_random_uniform(_top)	devrandom_secure_random_uniform((_top))
+
 #endif
 
 
--- src/secure_random.c
+++ src/secure_random.c
@@ -18,13 +18,22 @@
 
 #include <ilias/net2/bsd_compat/secure_random.h>
 
-#ifdef WIN32
+#ifdef HAS_ARC4RANDOM
+
+/*
+ * arc4random provides entropy, nothing needs to be implemented.
+ */
+
+#elif WIN32
+/*
+ * Window implementation.
+ */
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <ilias/net2/bsd_compat/error.h>
 #include <ilias/net2/bsd_compat/sysexits.h>
 
-ILIAS_NET2_EXPORT uint32_t
+ILIAS_NET2_LOCAL uint32_t
 win32_secure_random()
 {
 	uint32_t result;
@@ -33,12 +42,12 @@ win32_secure_random()
 	return result;
 }
 
-ILIAS_NET2_EXPORT void
+ILIAS_NET2_LOCAL void
 win32_secure_random_buf(void *ptr, size_t len)
 {
 	BOOLEAN (APIENTRY *pfn)(void*, ULONG);
 	HMODULE hLib;
-	
+
 	hLib = LoadLibrary("ADVAPI32.DLL");
 	if (!hLib)
 		err(EX_OSERR, "LoadLibrary ADVAPI32.DLL: error code %d", (int32_t)GetLastError());
@@ -56,7 +65,7 @@ win32_secure_random_buf(void *ptr, size_t len)
 	FreeLibrary(hLib);
 }
 
-ILIAS_NET2_EXPORT uint32_t
+ILIAS_NET2_LOCAL uint32_t
 win32_secure_random_uniform(uint32_t upper_bound)
 {
 	/*
@@ -95,5 +104,86 @@ win32_secure_random_uniform(uint32_t upper_bound)
 }
 
 #else
-#error "No implementation of secure random present."
+
+/*
+ * Use /dev/random.
+ */
+ILIAS_NET2_LOCAL uint32_t
+devrandom_secure_random()
+{
+	uint32_t result;
+
+	devrandom_secure_random_buf(&result, sizeof(result));
+	return result;
+}
+
+ILIAS_NET2_LOCAL void
+devrandom_secure_random_buf(void *ptr, size_t len)
+{
+	int	fd;
+	ssize_t	rd;
+
+	/* Open /dev/random. */
+	fd = open("/dev/random", O_RDONLY, 0);
+	if (fd == -1)
+		err(EX_OSERR, "failed to open /dev/random");
+
+	/* Read from /dev/random. */
+	while (len > 0) {
+		rd = read(fd, ptr, len);
+		if (rd == -1)
+			err(EX_OSERR, "failed to read from /dev/random");
+		else if (rd == 0)
+			err(EX_OSERR, "/dev/random depleted");
+		len -= rd;
+		ptr = (char*)ptr + rd;
+	}
+
+	/* Close /dev/random. */
+	while (close(fd)) {
+		if (errno != EINTR) {
+			warn(EX_OSERR, "failed to close /dev/random");
+			break;
+		}
+	}
+}
+
+ILIAS_NET2_LOCAL uint32_t
+devrandom_secure_random_uniform(uint32_t upper_bound)
+{
+	/*
+	 * Lifted from OpenBSD.
+	 */
+	uint32_t r, min;
+
+	if (upper_bound < 2)
+		return 0;
+
+#if (ULONG_MAX > 0xffffffffUL)
+	min = 0x100000000UL % upper_bound;
+#else
+	/* Calculate (2**32 % upper_bound) avoiding 64-bit math */
+	if (upper_bound > 0x80000000)
+		min = 1 + ~upper_bound;		/* 2**32 - upper_bound */
+	else {
+		/* (2**32 - (x * 2)) % x == 2**32 % x when x <= 2**31 */
+		min = ((0xffffffff - (upper_bound * 2)) + 1) % upper_bound;
+	}
+#endif
+
+	/*
+	 * This could theoretically loop forever but each retry has
+	 * p > 0.5 (worst case, usually far better) of selecting a
+	 * number inside the range we need, so it should rarely need
+	 * to re-roll.
+	 */
+	for (;;) {
+		r = secure_random();
+		if (r >= min)
+			break;
+	}
+
+	return r % upper_bound;
+}
+
 #endif
