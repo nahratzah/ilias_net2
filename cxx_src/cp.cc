eacc14049998e4d7aaed121e4069aae0ed686e37
ariane <ariane@stack.nl>
2012-11-01 23:30:09 +0100

    Change encoding/decoding of data over to c++.

---
+++ cxx_src/cp.cc
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2012 Ariane van der Steldt <ariane@stack.nl>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+#include <ilias/net2/cp.h>
+
+
+namespace ilias {
+namespace buf_encode {
+
+static constexpr std::size_t buf_padding = 8;	/* ABI: Ensure strings and bufs always use a multiple of 8 bytes of space. */
+typedef std::uint32_t buf_len_indicator;	/* ABI: Type used to indicate buf/string length. */
+
+/* Calculate how much padding is required. */
+inline constexpr buf_len_indicator
+buf_padding_length(buf_len_indicator len)
+{
+	/*
+	 * (x - 1) % y + 1  makes the modulo round up to y instead of down to 0.
+	 * Which makes  y - ((x - 1) % y + 1)  able to reach 0 instead of y.
+	 *
+	 * Below is what happens if you optimize it for constant folding.
+	 */
+	return (buf_padding - 1) - (sizeof(buf_len_indicator) - 1 + len) % buf_padding;
+}
+
+} /* namespace ilias::buf_encode */
+
+template<>
+void
+cp_encdec<std::string>::encode(encdec_ctx& ectx, buffer& out, const std::string& value)
+{
+	using namespace buf_encode;
+
+	const buf_len_indicator len = value.length();
+
+	char pad[buf_padding];
+	std::for_each(&pad[0], &pad[buf_padding], [](char& v) { v = '\0'; });
+
+	/* Encode string length. */
+	cp_encdec<buf_len_indicator>::encode(ectx, out, len);
+	/* Encode string payload. */
+	out.append(reinterpret_cast<const void*>(value.data()), len);
+	/* Pad until the combination is a multiple of buf_padding. */
+	out.append(reinterpret_cast<const void*>(&pad[0]), buf_padding_length(len));
+}
+template<>
+RVALUE(std::string)
+cp_encdec<std::string>::decode(encdec_ctx& ectx, buffer& in)
+{
+	using namespace buf_encode;
+
+	const buf_len_indicator len = cp_encdec<buf_len_indicator>::decode(ectx, in);
+	const buf_len_indicator padding = buf_padding_length(len);
+
+	/* Read string from buffer, using the visitor pattern. */
+	std::string rv;
+	in.visit([&rv](const void* p, buffer::size_type l) {
+		const char* b = reinterpret_cast<const char*>(p);
+		rv.append(b, l);
+	});
+
+	/* Visitor doesn't drain, so we must do so here. */
+	in.drain(len + padding);
+
+	return MOVE(rv);
+}
+
+template<>
+void
+cp_encdec<buffer>::encode(encdec_ctx& ectx, buffer& out, const buffer& value)
+{
+	using namespace buf_encode;
+
+	/* Ensure this will fit. */
+	if (value.size() > std::numeric_limits<buf_len_indicator>::max())
+		throw std::domain_error("buffer is too large to be encoded");
+
+	const buf_len_indicator len = value.size();
+
+	char pad[buf_padding];
+	std::for_each(&pad[0], &pad[buf_padding], [](char& v) { v = '\0'; });
+
+	/* Encode buffer length. */
+	cp_encdec<buf_len_indicator>::encode(ectx, out, len);
+	/* Encode buffer payload. */
+	out += value;
+	/* Pad until the combination is a multiple of buf_padding. */
+	out.append(reinterpret_cast<const void*>(&pad[0]), buf_padding_length(len));
+}
+template<>
+RVALUE(buffer)
+cp_encdec<buffer>::decode(encdec_ctx& ectx, buffer& in)
+{
+	using namespace buf_encode;
+
+	const buf_len_indicator len = cp_encdec<buf_len_indicator>::decode(ectx, in);
+	const buf_len_indicator padding = buf_padding_length(len);
+
+	/* Read string from buffer, using the visitor pattern. */
+	buffer rv = in.subrange(0, len);
+	/* buffer::subrange doesn't drain, so we must do so here. */
+	in.drain(len + padding);
+
+	return MOVE(rv);
+}
+
+
+} /* namespace ilias */
--- include/ilias/net2/cp.h
+++ include/ilias/net2/cp.h
@@ -19,72 +19,301 @@
 #include <ilias/net2/ilias_net2_export.h>
 #include <ilias/net2/types.h>
 #include <ilias/net2/buffer.h>
+#include <cstdint>
+#include <string>
+#ifdef HAVE_TYPE_TRAITS
+#include <type_traits>
+#endif
 
-ILIAS_NET2__begin_cdecl
+
+namespace ilias {
 
 
-typedef int (*net2_cp_encfun) (struct net2_encdec_ctx*,
-    struct net2_buffer*, const void*, const void*);
-typedef int (*net2_cp_decfun) (struct net2_encdec_ctx*,
-    void*, struct net2_buffer*, const void*);
-typedef int (*net2_cp_initfun) (void*, const void*);
-typedef int (*net2_cp_delfun) (void*, const void*);
+class encdec_ctx;
 
-struct command_param {
-	int		 cp_flags;
-#define CPT_NONE	 (0)
-	size_t		 cp_size;
-	const char	*cp_name;
-	net2_cp_encfun	 cp_encode;
-	net2_cp_decfun	 cp_decode;
-	net2_cp_initfun	 cp_init;
-	net2_cp_delfun	 cp_delete;
+
+/*
+ * Interface for encoding/decoding.
+ *
+ * Can be specialized and/or implemented by types.
+ */
+template<typename T>
+struct cp_encdec
+{
+	typedef T value_type;
+	typedef value_type& reference;
+	typedef const value_type& const_reference;
+	typedef value_type* pointer;
+	typedef const value_type* const_pointer;
+	typedef RVALUE(value_type) result_type;
+
+	static void encode(encdec_ctx&, buffer&, const_reference);
+	static result_type decode(encdec_ctx&, buffer&);
 };
 
-ILIAS_NET2_EXPORT
-int net2_cp_encode(struct net2_encdec_ctx*, const struct command_param*,
-    struct net2_buffer*, const void*, const void*);
-ILIAS_NET2_EXPORT
-int net2_cp_decode(struct net2_encdec_ctx*, const struct command_param*,
-    void*, struct net2_buffer*, const void*);
-ILIAS_NET2_EXPORT
-int net2_cp_init(const struct command_param*, void*, const void*);
-ILIAS_NET2_EXPORT
-int net2_cp_destroy(const struct command_param*, void*, const void*);
-ILIAS_NET2_EXPORT
-int net2_cp_init_alloc(const struct command_param*,
-    void**, const void*);
-ILIAS_NET2_EXPORT
-int net2_cp_destroy_alloc(const struct command_param*,
-    void**, const void*);
+
+namespace endian_detail {
 
 
-struct net2_objmanager;	/* From ilias/net2/obj_manager.h */
-struct net2_promise;	/* From ilias/net2/promise.h */
+#ifdef BIG_ENDIAN
+template<typename T>
+T
+big_endian(const T& v) ILIAS_NET2_NOTHROW
+{
+	return v;
+}
+template<typename T>
+T
+host_endian(const T& v) ILIAS_NET2_NOTHROW
+{
+	return v;
+}
+#else
+/* Assuming little endian. */
 
-typedef void (*net2_cm_invocation) (struct net2_promise*,
-    struct net2_encdec_ctx *ctx, void*);
+template<typename T, int B = 0, bool Stop = 2 * B + 1 >= sizeof(T)>
+struct endian_helper
+{
+private:
+	static constexpr unsigned int byte_digits = std::numeric_limits<uint8_t>::digits;
+	static constexpr unsigned int t_digits = std::numeric_limits<T>::digits;
+	/* Mask the lowest byte in T. */
+	static constexpr T mask_byte = ((T(1) << byte_digits) - 1);
 
-struct command_method {
-	const struct net2_protocol
-			*cm_protocol;	/* Protocol of request. */
-	const struct command_param
-			*cm_in;		/* Input type. */
-	const struct command_param
-			*cm_out;	/* Output type. */
-	int		 cm_flags;	/* Command method options. */
-#define CM_ASYNC	0x00000001	/* Asynchronous method. */
-#define CM_BARRIER_PRE	0x00000002	/* Raise barrier prior. */
-#define CM_BARRIER_POST	0x00000004	/* Raise barrier after. */
-	net2_cm_invocation
-			 cm_method;	/* Implementation function. */
+	/* Mask the low byte we want to swap. */
+	static constexpr T low_mask = (mask_byte << (B * byte_digits));
+	/* Mask the high byte we want to swap. */
+	static constexpr T high_mask = (mask_byte << (t_digits - byte_digits - B * byte_digits));
+	/* Mask for the bits we do nothing with. */
+	static constexpr T not_mask = ~(low_mask | high_mask);
+
+	/* Calculate how far both parts need to be shifted. */
+	static constexpr unsigned int distance = t_digits - byte_digits - 2 * byte_digits * B;
+
+	/* Flip the byte B in T from byte order. */
+	static constexpr T
+	flip(T v) ILIAS_NET2_NOTHROW
+	{
+		return ((v & low_mask) << distance) | ((v & high_mask) >> distance) | (v & not_mask);
+	}
+
+public:
+	/* Recursively flip the byte order in T. */
+	static constexpr T
+	flip_endian(T v) ILIAS_NET2_NOTHROW
+	{
+		return endian_helper<T, B + 1>::flip_endian(flip(v));
+	}
 };
 
-ILIAS_NET2_EXPORT
-struct net2_promise
-		*net2_invoke(struct net2_objmanager*,
-		    const struct command_method*, void*);
+template<typename T, int B>
+struct endian_helper<T, B, true>
+{
+public:
+	/* Byte B cannot be flipped, therefore this is the identity operation. */
+	static constexpr T
+	flip_endian(T v) ILIAS_NET2_NOTHROW
+	{
+		return v;
+	}
+};
+
+template<typename T>
+T
+big_endian(const T& v)
+{
+	return endian_helper<T>::flip_endian(v);
+}
+template<typename T>
+T
+host_endian(const T& v)
+{
+	return endian_helper<T>::flip_endian(v);
+}
+#endif
 
 
-ILIAS_NET2__end_cdecl
+/*
+ * Wrt two's complement, we assume your platform uses it.
+ * The functions in this namespace will allow for abstraction should a platform be found that doesn't do this.
+ */
+
+/* Encode signed value v as two's complement value. */
+template<typename T>
+typename std::make_unsigned<T>::type
+net_two_compl(const typename std::make_signed<T>::type& v)
+{
+	return reinterpret_cast<const typename std::make_unsigned<T>::type&>(v);
+}
+template<typename T>
+typename std::make_signed<T>::type
+host_two_compl(const typename std::make_unsigned<T>::type& v)
+{
+	return reinterpret_cast<const typename std::make_signed<T>::type&>(v);
+}
+
+
+} /* namespace ilias::endian_detail */
+
+
+template<>
+inline void
+cp_encdec<std::uint8_t>::encode(encdec_ctx& ectx, buffer& out, const std::uint8_t& value)
+{
+	using namespace endian_detail;
+
+	out.append_literal(value);
+}
+template<>
+inline RVALUE(std::uint8_t)
+cp_encdec<std::uint8_t>::decode(encdec_ctx& ectx, buffer& in)
+{
+	using namespace endian_detail;
+
+	return MOVE(in.drain_literal<uint8_t>());
+}
+
+template<>
+inline void
+cp_encdec<std::uint16_t>::encode(encdec_ctx& ectx, buffer& out, const std::uint16_t& value)
+{
+	using namespace endian_detail;
+
+	out.append_literal(big_endian(value));
+}
+template<>
+inline RVALUE(std::uint16_t)
+cp_encdec<std::uint16_t>::decode(encdec_ctx& ectx, buffer& in)
+{
+	using namespace endian_detail;
+
+	return MOVE(host_endian(in.drain_literal<std::uint16_t>()));
+}
+
+template<>
+inline void
+cp_encdec<std::uint32_t>::encode(encdec_ctx& ectx, buffer& out, const std::uint32_t& value)
+{
+	using namespace endian_detail;
+
+	out.append_literal(big_endian(value));
+}
+template<>
+inline RVALUE(std::uint32_t)
+cp_encdec<std::uint32_t>::decode(encdec_ctx& ectx, buffer& in)
+{
+	using namespace endian_detail;
+
+	return MOVE(host_endian(in.drain_literal<std::uint32_t>()));
+}
+
+template<>
+inline void
+cp_encdec<std::uint64_t>::encode(encdec_ctx& ectx, buffer& out, const std::uint64_t& value)
+{
+	using namespace endian_detail;
+
+	out.append_literal(big_endian(value));
+}
+template<>
+inline RVALUE(std::uint64_t)
+cp_encdec<std::uint64_t>::decode(encdec_ctx& ectx, buffer& in)
+{
+	using namespace endian_detail;
+
+	return MOVE(host_endian(in.drain_literal<std::uint64_t>()));
+}
+
+
+template<>
+inline void
+cp_encdec<std::int8_t>::encode(encdec_ctx& ectx, buffer& out, const std::int8_t& value)
+{
+	using namespace endian_detail;
+
+	cp_encdec<std::uint8_t>::encode(ectx, out, net_two_compl<std::uint8_t>(value));
+}
+template<>
+inline RVALUE(std::int8_t)
+cp_encdec<std::int8_t>::decode(encdec_ctx& ectx, buffer& in)
+{
+	using namespace endian_detail;
+
+	return MOVE(host_two_compl<std::uint8_t>(cp_encdec<std::uint8_t>::decode(ectx, in)));
+}
+
+template<>
+inline void
+cp_encdec<std::int16_t>::encode(encdec_ctx& ectx, buffer& out, const std::int16_t& value)
+{
+	using namespace endian_detail;
+
+	cp_encdec<std::uint16_t>::encode(ectx, out, net_two_compl<std::uint16_t>(value));
+}
+template<>
+inline RVALUE(std::int16_t)
+cp_encdec<std::int16_t>::decode(encdec_ctx& ectx, buffer& in)
+{
+	using namespace endian_detail;
+
+	return MOVE(host_two_compl<std::uint16_t>(cp_encdec<std::uint16_t>::decode(ectx, in)));
+}
+
+template<>
+inline void
+cp_encdec<std::int32_t>::encode(encdec_ctx& ectx, buffer& out, const std::int32_t& value)
+{
+	using namespace endian_detail;
+
+	cp_encdec<std::uint32_t>::encode(ectx, out, net_two_compl<std::uint32_t>(value));
+}
+template<>
+inline RVALUE(std::int32_t)
+cp_encdec<std::int32_t>::decode(encdec_ctx& ectx, buffer& in)
+{
+	using namespace endian_detail;
+
+	return MOVE(host_two_compl<std::uint32_t>(cp_encdec<std::uint32_t>::decode(ectx, in)));
+}
+
+template<>
+inline void
+cp_encdec<std::int64_t>::encode(encdec_ctx& ectx, buffer& out, const std::int64_t& value)
+{
+	using namespace endian_detail;
+
+	cp_encdec<std::uint64_t>::encode(ectx, out, net_two_compl<std::uint64_t>(value));
+}
+template<>
+inline RVALUE(std::int64_t)
+cp_encdec<std::int64_t>::decode(encdec_ctx& ectx, buffer& in)
+{
+	using namespace endian_detail;
+
+	return MOVE(host_two_compl<std::uint64_t>(cp_encdec<std::uint64_t>::decode(ectx, in)));
+}
+
+template<>
+ILIAS_NET2_EXPORT void
+cp_encdec<std::string>::encode(encdec_ctx& ectx, buffer& out, const std::string& value);
+template<>
+ILIAS_NET2_EXPORT RVALUE(std::string)
+cp_encdec<std::string>::decode(encdec_ctx& ectx, buffer& in);
+
+template<>
+ILIAS_NET2_EXPORT void
+cp_encdec<buffer>::encode(encdec_ctx& ectx, buffer& out, const buffer& value);
+template<>
+ILIAS_NET2_EXPORT RVALUE(buffer)
+cp_encdec<buffer>::decode(encdec_ctx& ectx, buffer& in);
+
+extern template void cp_encdec<std::string>::encode(encdec_ctx& ectx, buffer& out, const std::string& value);
+extern template RVALUE(std::string) cp_encdec<std::string>::decode(encdec_ctx& ectx, buffer& in);
+
+extern template void cp_encdec<buffer>::encode(encdec_ctx& ectx, buffer& out, const buffer& value);
+extern template RVALUE(buffer) cp_encdec<buffer>::decode(encdec_ctx& ectx, buffer& in);
+
+
+} /* namespace ilias */
+
 #endif /* ILIAS_NET2_CP_H */
