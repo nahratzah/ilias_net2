55a4c5f5582ef45881255739c9ecddc8ac4b4f63
Ariane van der Steldt <ariane@stack.nl>
2012-11-19 02:08:51 +0100

    Initial work to support threadpool suicide.

--- cxx_src/threadpool.cc
+++ cxx_src/threadpool.cc
@@ -3,15 +3,26 @@
 namespace ilias {
 
 
+threadpool::thread*&
+threadpool::thread::tls_self() ILIAS_NET2_NOTHROW
+{
+#ifdef WIN32
+#define thread_local __declspec(thread)
+#endif
+	static thread_local threadpool::thread* tls;
+	return tls;
+}
+
+
 threadpool::threadpool(std::function<bool()> pred, std::function<bool()> worker, unsigned int threads) :
-	m_factory([pred, worker](threadpool& self) -> thread_ptr {
-		return thread_ptr(new thread(self, pred, worker));
+	m_factory([pred, worker](threadpool& self, unsigned int idx) -> thread_ptr {
+		return thread_ptr(new thread(self, idx, pred, worker));
 	    }),
 	m_idle(new idle_threads),
 	m_all()
 {
 	for (unsigned int i = 0; i < threads; ++i)
-		m_all.push_back(m_factory(*this));
+		m_all.push_back(m_factory(*this, i));
 }
 
 /*
@@ -19,32 +30,76 @@ threadpool::threadpool(std::function<bool()> pred, std::function<bool()> worker,
  * (and I'm sure many other compilers lack this as well).
  */
 threadpool::threadpool(std::function<bool()> pred, std::function<bool()> worker) :
-	m_factory([pred, worker](threadpool& self) -> thread_ptr {
-		return thread_ptr(new thread(self, pred, worker));
+	m_factory([pred, worker](threadpool& self, unsigned int idx) -> thread_ptr {
+		return thread_ptr(new thread(self, idx, pred, worker));
 	    }),
 	m_idle(new idle_threads),
 	m_all()
 {
 	const unsigned int threads = std::max(1U, std::thread::hardware_concurrency());
 	for (unsigned int i = 0; i < threads; ++i)
-		m_all.push_back(m_factory(*this));
+		m_all.push_back(m_factory(*this, i));
+}
+
+bool
+threadpool::curthread_is_threadpool() ILIAS_NET2_NOTHROW
+{
+	const thread* t = thread::tls_self();
+	return (t && &t->m_idle == this->m_idle.get());
 }
 
 threadpool::~threadpool() ILIAS_NET2_NOTHROW
 {
 	for (auto& thr : m_all) {
-		thr->kill();
-		thr->join();
+		switch (thr->kill()) {
+		case thread::KILL_OK:
+			thr->join();
+			break;
+		case thread::KILL_SUICIDE:
+			thr.release();	/* Thread will free itself. */
+			break;
+		default:
+			std::terminate();
+		}
 	}
 	this->m_all.clear();
 }
 
 
-threadpool::thread::thread(threadpool& tp, const std::function<bool()>& pred, const std::function<bool()>& worker) :
+class threadpool::thread::publish_idle
+{
+private:
+	thread& m_self;
+
+public:
+	publish_idle(thread& s) ILIAS_NET2_NOTHROW :
+		m_self(s)
+	{
+		this->m_self.m_idle.push_back(this->m_self);
+	}
+
+	~publish_idle() ILIAS_NET2_NOTHROW
+	{
+		this->m_self.m_idle.erase_element(this->m_self.m_idle.iterator_to(this->m_self));
+	}
+
+
+#if HAS_DELETED_FN
+	publish_idle(const publish_idle&) = delete;
+	publish_idle& operator=(const publish_idle&) = delete;
+#else
+private:
+	publish_idle(const publish_idle&);
+	publish_idle& operator=(const publish_idle&);
+#endif
+};
+
+threadpool::thread::thread(threadpool& tp, unsigned int idx, const std::function<bool()>& pred, const std::function<bool()>& worker) :
 	m_state(STATE_ACTIVE),
 	m_sleep_mtx(),
 	m_wakeup(),
 	m_idle(*tp.m_idle),
+	m_idx(idx),
 	m_self(&thread::run, this, pred, worker)
 {
 	/* Empty body. */
@@ -87,19 +142,45 @@ threadpool::thread::do_sleep(const std::function<bool()>& pred) ILIAS_NET2_NOTHR
 
 	/* Ensure state is valid on exit. */
 	const auto end_state = this->get_state();
-	assert(end_state == STATE_ACTIVE || end_state == STATE_DYING);
+	assert(end_state == STATE_ACTIVE || end_state == STATE_DYING || end_state == STATE_SUICIDE);
 }
 
-void
+std::unique_ptr<threadpool::thread>
 threadpool::thread::run(const std::function<bool()>& pred, const std::function<bool()>& functor)
 {
-	for (;;) {
-		do {
-			if (this->get_state() == STATE_DYING)
-				return;
-		} while (functor());
-		do_sleep(pred);
+	std::unique_ptr<threadpool::thread> cyanide;	/* Used to pass this to parent in order for it to clean up. */
+
+	struct tls_storage
+	{
+		tls_storage(thread* self) ILIAS_NET2_NOTHROW
+		{
+			assert(self);
+			assert(!tls_self());
+			tls_self() = self;
+		}
+
+		~tls_storage() ILIAS_NET2_NOTHROW
+		{
+			assert(tls_self());
+			tls_self() = nullptr;
+		}
+	};
+
+
+	{
+		/* Publish thread, so we can detect suicide cases. */
+		tls_storage identification(this);
+
+		while (this->get_state() == STATE_ACTIVE) {
+			if (!functor())
+				do_sleep(pred);
+		}
 	}
+
+	/* Swallow the pill to kill ourselves. */
+	if (this->get_state() == STATE_SUICIDE)
+		cyanide.reset(this);
+	return cyanide;
 }
 
 bool
@@ -124,10 +205,18 @@ threadpool::thread::wakeup() ILIAS_NET2_NOTHROW
 	return true;
 }
 
-bool
+threadpool::thread::kill_result
 threadpool::thread::kill() ILIAS_NET2_NOTHROW
 {
-	int pstate = this->m_state.exchange(STATE_DYING, std::memory_order_release);
+	const int setstate = (this->get_id() == std::this_thread::get_id() ?
+	    STATE_SUICIDE : STATE_DYING);
+
+	int pstate = STATE_ACTIVE;
+	while (!this->m_state.compare_exchange_weak(pstate, setstate,
+	    std::memory_order_release, std::memory_order_relaxed)) {
+		if (pstate == STATE_DYING || pstate == STATE_SUICIDE)
+			return KILL_TWICE;
+	}
 
 	/* Signal wakeup. */
 	if (pstate == STATE_SLEEP) {
@@ -136,12 +225,16 @@ threadpool::thread::kill() ILIAS_NET2_NOTHROW
 		this->m_wakeup.notify_one();
 	}
 
-	return (pstate != STATE_DYING);
+	if (setstate == STATE_SUICIDE)
+		this->m_self.detach();	/* Will destroy this on thread finish. */
+
+	return (setstate == STATE_SUICIDE ? KILL_SUICIDE : KILL_OK);
 }
 
 void
 threadpool::thread::join() ILIAS_NET2_NOTHROW
 {
+	assert(this->get_state() == STATE_DYING);
 	this->m_self.join();
 }
 
--- include/ilias/net2/threadpool.h
+++ include/ilias/net2/threadpool.h
@@ -20,7 +20,7 @@ private:
 	typedef std::vector<thread_ptr> all_threads;
 	typedef ll_list<ll_base<thread, idle_tag> > idle_threads;
 
-	std::function<thread_ptr(threadpool&)> m_factory;
+	std::function<thread_ptr(threadpool&, unsigned int)> m_factory;
 	std::unique_ptr<idle_threads> m_idle;
 	all_threads m_all;
 
@@ -35,6 +35,8 @@ public:
 	{
 		/* Empty body. */
 	}
+
+	ILIAS_NET2_EXPORT bool curthread_is_threadpool() ILIAS_NET2_NOTHROW;
 
 	ILIAS_NET2_EXPORT ~threadpool() ILIAS_NET2_NOTHROW;
 
@@ -55,46 +57,39 @@ private:
 class ILIAS_NET2_LOCAL threadpool::thread FINAL :
 	public ll_base_hook<threadpool::idle_tag>
 {
-private:
+public:
+	/*
+	 * DFA.  Allowed transisitions:
+	 *             -> { ACTIVE }
+	 * ACTIVE      -> { SLEEP_TEST, DYING, SUICIDE }
+	 * SLEEP_TEST  -> { ACTIVE, SLEEP, DYING, SUICIDE }
+	 * SLEEP       -> { ACTIVE, DYING, SUICIDE }
+	 * DYING       -> {  }
+	 * SUICIDE     -> {  }
+	 */
 	static CONSTEXPR_VALUE int STATE_ACTIVE = 0;
 	static CONSTEXPR_VALUE int STATE_SLEEP_TEST = 1;
 	static CONSTEXPR_VALUE int STATE_SLEEP = 2;
-	static CONSTEXPR_VALUE int STATE_DYING = 0xff;
+	static CONSTEXPR_VALUE int STATE_DYING = 0xff;		/* Worker died and needs to be joined. */
+	static CONSTEXPR_VALUE int STATE_SUICIDE = 0xfe;	/* Worker killed itself and detached. */
+
+	enum kill_result {
+		KILL_TWICE,	/* Was already dying. */
+		KILL_OK,	/* Was killed by current invocation. */
+		KILL_SUICIDE	/* Call to kill was suicide. */
+	};
+
+private:
+	class publish_idle;
 
 	std::atomic<int> m_state;
 	std::mutex m_sleep_mtx;
 	std::condition_variable m_wakeup;
+
+public:
 	idle_threads& m_idle;
+	const unsigned int m_idx;
 	std::thread m_self;	/* Must be the last variable in this class. */
-
-	class publish_idle
-	{
-	private:
-		thread& m_self;
-
-	public:
-		publish_idle(thread& s) ILIAS_NET2_NOTHROW :
-			m_self(s)
-		{
-			this->m_self.m_idle.push_back(this->m_self);
-		}
-
-		~publish_idle() ILIAS_NET2_NOTHROW
-		{
-			this->m_self.m_idle.erase_element(this->m_self.m_idle.iterator_to(this->m_self));
-		}
-
-
-#if HAS_DELETED_FN
-		publish_idle(const publish_idle&) = delete;
-		publish_idle& operator=(const publish_idle&) = delete;
-#else
-	private:
-		publish_idle(const publish_idle&);
-		publish_idle& operator=(const publish_idle&);
-#endif
-	};
-
 
 	int
 	get_state() const ILIAS_NET2_NOTHROW
@@ -102,15 +97,24 @@ private:
 		return this->m_state.load(std::memory_order_relaxed);
 	}
 
+private:
 	void do_sleep(const std::function<bool()>& pred) ILIAS_NET2_NOTHROW;
-	void run(const std::function<bool()>& pred, const std::function<bool()>& functor);
+	std::unique_ptr<threadpool::thread> run(const std::function<bool()>& pred, const std::function<bool()>& functor);
 
 public:
-	thread(threadpool& tp, const std::function<bool()>& pred, const std::function<bool()>& worker);
+	thread(threadpool& tp, unsigned int idx, const std::function<bool()>& pred, const std::function<bool()>& worker);
 	bool wakeup() ILIAS_NET2_NOTHROW;
-	bool kill() ILIAS_NET2_NOTHROW;
+	kill_result kill() ILIAS_NET2_NOTHROW;
 	void join() ILIAS_NET2_NOTHROW;
 	~thread() ILIAS_NET2_NOTHROW;
+
+	static thread*& tls_self() ILIAS_NET2_NOTHROW;
+
+	std::thread::id
+	get_id() const ILIAS_NET2_NOTHROW
+	{
+		return this->m_self.get_id();
+	}
 
 
 #if HAS_DELETED_FN
