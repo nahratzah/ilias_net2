7c99afb6fb8bbbf1ba6297f7808fafed7608a2b0
Ariane van der Steldt <ariane@stack.nl>
2012-12-03 16:46:20 +0100

    Also test specialized constructor in constructor traits test.

--- CMake/source/std_constructor_traits.cc
+++ CMake/source/std_constructor_traits.cc
@@ -1,12 +1,17 @@
 #include <type_traits>
 
-class foo{};
+class foo
+{
+	foo();
+	foo(int);
+};
 
 int
 main()
 {
 	/* Test if these templates instantiate correctly. */
 	bool test =
+	    std::is_nothrow_constructible<foo, int>::value &&
 	    std::is_nothrow_destructable<foo>::value &&
 	    std::is_nothrow_copy_constructable<foo>::value &&
 	    std::is_nothrow_move_constructable<foo>::value;
